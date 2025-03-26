946d7046a0481394dab43a7219636f1e67877f20
derlyalfonsor <derly.alfonso-r@mail.escuelaing.edu.co>
2025-03-26 16:34:40 -0500

    cambios system y evalue

--- app/app.py
+++ app/app.py
@@ -23,6 +23,16 @@ async def get_patient_by_id(patient_id: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
+@app.get("/patient/{patient_id}", response_model=dict)
+async def get_patient_by_identifier(system: str, value:str):
+    status,patient = GetPatientByIdentifier(patient_id)
+    if status=='success':
+        return patient  # Return patient
+    elif status=='notFound':
+        raise HTTPException(status_code=404, detail="Patient not found")
+    else:
+        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
+
 
 @app.post("/patient", response_model=dict)
 async def add_patient(request: Request):
